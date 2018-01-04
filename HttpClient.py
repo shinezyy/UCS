040424a5441f3ae959bfb7da301424d74c389a49
Zhou Yaoyang <diamondzyy@sina.com>
2018-01-04 16:52:25 +0800

    port it to python3

--- HttpClient.py
+++ HttpClient.py
@@ -5,35 +5,37 @@ FileName : HttpClient.py
 Author   : septicmk
 """
 
-import cookielib, urllib, urllib2, socket, urlparse
+import http.cookiejar, urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse, socket, urllib.parse
 import os, sys
+from urllib.parse import quote
+
 
 class HttpClient:
-  __cookie = cookielib.CookieJar()
-  __req = urllib2.build_opener(urllib2.HTTPCookieProcessor(__cookie))
+  __cookie = http.cookiejar.CookieJar()
+  __req = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(__cookie))
   __req.addheaders = [
     #('Accept', 'application/javascript, */*;q=0.8'),
     ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
     ('User-Agent', "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36")
   ]
-  urllib2.install_opener(__req)
+  urllib.request.install_opener(__req)
 
   def Get(self, url, refer=None):
     try:
-      req = urllib2.Request(url.replace(' ','%20'))
+      req = urllib.request.Request(url.replace(' ','%20'))
       if not (refer is None):
           req.add_header('Referer', refer)
-      return urllib2.urlopen(req).read()
-    except urllib2.HTTPError, e:
+      return urllib.request.urlopen(req).read()
+    except urllib.error.HTTPError as e:
       return e.read()
 
   def Post(self, url, data, refer=None):
     try:
-      req = urllib2.Request(url, urllib.urlencode(data))
+      req = urllib.request.Request(url, urllib.parse.urlencode(data).encode('utf-8'))
       if not (refer is None):
         req.add_header('Referer', refer)
-      return urllib2.urlopen(req, timeout=180).read()
-    except urllib2.HTTPError, e:
+      return urllib.request.urlopen(req, timeout=180).read()
+    except urllib.error.HTTPError as e:
       return e.read()
 
   def Download(self, url, file):
@@ -63,22 +65,24 @@ class HttpClient:
             if not chunk:
                 break
     
-            if report_hook:
-                report_hook(bytes_so_far, chunk_size, total_size)
+            # if report_hook:
+            #     report_hook(bytes_so_far, chunk_size, total_size)
         return ret
 
     output = open(file, 'wb')
-    try:
-        response = urllib2.urlopen(url)
-        content = chunk_read(response, report_hook=chunk_report)
-        output.write(content)
-    except:
-        parsed_link = urlparse.urlsplit(url.encode('utf8'))
-        parsed_link = parsed_link._replace(path=urllib.quote(parsed_link.path))
-        url = parsed_link.geturl()
-        response = urllib2.urlopen(url)
-        content = chunk_read(response, report_hook=chunk_report)
-        output.write(content)
+    # try:
+    print(url)
+    url = quote(url, safe='/:?=')
+    response = urllib.request.urlopen(url)
+    content = chunk_read(response, report_hook=chunk_report)
+    output.write(content)
+    # except:
+    #     parsed_link = urllib.parse.urlsplit(url.encode('utf8'))
+    #     parsed_link = parsed_link._replace(path=urllib.parse.quote(parsed_link.path))
+    #     url = parsed_link.geturl()
+    #     response = urllib.request.urlopen(url).read()
+    #     content = chunk_read(response, report_hook=chunk_report)
+    #     output.write(content)
 
     output.close()
 
@@ -92,6 +96,6 @@ class HttpClient:
     return ''
 
   def setCookie(self, key, val, domain):
-    ck = cookielib.Cookie(version=0, name=key, value=val, port=None, port_specified=False, domain=domain, domain_specified=False, domain_initial_dot=False, path='/', path_specified=True, secure=False, expires=None, discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
+    ck = http.cookiejar.Cookie(version=0, name=key, value=val, port=None, port_specified=False, domain=domain, domain_specified=False, domain_initial_dot=False, path='/', path_specified=True, secure=False, expires=None, discard=True, comment=None, comment_url=None, rest={'HttpOnly': None}, rfc2109=False)
     self.__cookie.set_cookie(ck)
 #self.__cookie.clear() clean cookie
--- syncc.py
+++ syncc.py
@@ -9,11 +9,13 @@ Author   : septicmk
 from HttpClient import *
 import re
 import logging
-import ConfigParser
+import configparser
 import getopt
 import sys
+from importlib import reload
+from html import unescape
 reload(sys)
-sys.setdefaultencoding('utf-8')
+# sys.setdefaultencoding('utf-8')
 
 force_update_flag = False
 
@@ -25,21 +27,23 @@ logging.basicConfig(
 )
 
 def usage():
-    print '''
+    print('''
 [options]
     -f, --force-update      Download all files and overwrite the existed.
                             Useful when teachers have updated original files.
                             ATTENTION: Causing heavy net traffic.
-'''
+''')
 
 def check_existed(path):
     import os
     return os.path.exists(path)
 
 def pause():
-    raw_input('press \'Enter\' to continue')
+    eval(input('press \'Enter\' to continue'))
 
 def get_revalue(html, rex, er, ex):
+    if type(html) is not str:
+        html = html.decode('utf-8')
     v = re.search(rex, html)
     if v is None:
         if ex:
@@ -56,13 +60,13 @@ class Course:
         self.hosturl = 'http://sep.ucas.ac.cn'
         self.loginurl = 'http://sep.ucas.ac.cn/slogin'
         self.req = HttpClient()
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read('./config.ini')
         self.usrname =  config.get('USER','usrname')
         self.passwd = config.get('USER', 'passwd')
         self.pwd = config.get('USER', 'savedir')
         if not (self.usrname and self.passwd and self.pwd):
-            print 'Please setup your account in \'config.ini\''
+            print('Please setup your account in \'config.ini\'')
             pause()
             sys.exit()
 
@@ -90,17 +94,17 @@ class Course:
         logging.debug("guid=" + guid)
 
         html = self.req.Get("http://course.ucas.ac.cn/portal?sakai.session={0}&_mid={1}".format(session, _mid))
-        course_urls = re.findall(r'http://course.ucas.ac.cn/portal/site/\d+', html)
+        course_urls = re.findall(r'http://course.ucas.ac.cn/portal/site/\d+', html.decode('utf-8'))
         #return course_urls
         for test_url in course_urls:
             self.get_resource(test_url)
 
 
     def get_resource(self, url):
-        import HTMLParser
+        import html.parser
         import os
 
-        html_parser = HTMLParser.HTMLParser()
+        html_parser = html.parser.HTMLParser()
         html = self.req.Get(url)
         res_url = get_revalue(html, r'class="icon-sakai-resources" href="(.+?)"', 'get resource error', 1)
         logging.debug("res_url= "+res_url)
@@ -108,22 +112,22 @@ class Course:
         wtf_url = get_revalue(html, r'http://course.ucas.ac.cn/portal/tool-reset/(.+?)/', 'what the fuxk error', 1)
         logging.debug("wtf_url= "+wtf_url)
         html = self.req.Get("http://course.ucas.ac.cn/portal/tool-reset/{0}/?panel=Main".format(wtf_url))
-        html = html_parser.unescape(html)
+        html = unescape(html.decode('utf-8'))
         #logging.debug(html)
 
         title = get_revalue(html, r'<img src =.*?/>([\s\S]+?)</h3>', 'get title error', 1).strip().replace(' ','_')
 
         logging.debug(title)
 
-        folders = filter(lambda x: 'doNavigate' in x,re.findall(r'onclick="javascript[\s\S]+?submit', html))
-        folders = map(lambda x: re.findall(r'group/\d+/[\s\S]+?/', x) , folders)[1:]
-        folders = map(lambda x: '/'+x[0], folders)
+        folders = [x for x in re.findall(r'onclick="javascript[\s\S]+?submit', html) if 'doNavigate' in x]
+        folders = [re.findall(r'group/\d+/[\s\S]+?/', x) for x in folders][1:]
+        folders = ['/'+x[0] for x in folders]
         #print folders
 
         res = re.findall(r'http://course.ucas.ac.cn/access/content/group/[^"]+', html)
-        res = map(lambda x: x[:-1] if x.endswith(";") else x, res)
+        res = [x[:-1] if x.endswith(";") else x for x in res]
         res = list(set(res))
-        logging.debug(html)
+        # logging.debug(html)
         self.download(title, res)
 
 
@@ -133,7 +137,7 @@ class Course:
                 'criteria':'title',
                 'sakai_action': 'doNavigate'}
             content = self.req.Post("http://course.ucas.ac.cn/portal/tool/{0}/?panel=Main".format(wtf_url), postData)
-            tmp = re.findall(r'http://course.ucas.ac.cn/access/content/group/[^"]+', content)
+            tmp = re.findall(r'http://course.ucas.ac.cn/access/content/group/[^"]+', content.decode('utf-8'))
             tmp = list(set(tmp))
             name = get_revalue(folder, r'([^/]+?)/$', 'folder error', 1).replace(' ', '_')
             #print name
@@ -143,7 +147,7 @@ class Course:
 
     def download(self, title, res):
         import os
-        print 'Linking' + '  ' + title
+        print(('Linking' + '  ' + title))
         logging.info('linking... ' + title)
         if not os.path.exists(self.pwd):
             os.makedirs(self.pwd)
@@ -163,7 +167,7 @@ class Course:
 
             __pwd = os.path.join(_pwd, name)
             if (not force_update_flag) and check_existed(__pwd):
-                print 'skip' + '  ' + name + ' already exists, skip'
+                print(('skip' + '  ' + name + ' already exists, skip'))
                 logging.info( name + ' already exists, skip')
                 continue
 
@@ -181,8 +185,8 @@ if __name__ == '__main__':
                 sys.exit()
             if name in ("-f","--force-update"):
                 force_update_flag = True
-    except getopt.GetoptError, err:
-        print str(err)
+    except getopt.GetoptError as err:
+        print((str(err)))
         usage()
         pause()
         sys.exit(2)
